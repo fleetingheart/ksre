40c08b94f562cb74a8c861b64ec4c37f77031c53
IkariyaBiorante <ikariyabiorante@noreply.codeberg.org>
2024-04-25 19:14:18 +0300

    build(pypresence): bump pypresence

--- game/python-packages/pypresence/utils.py
+++ game/python-packages/pypresence/utils.py
@@ -9,7 +9,6 @@ import time
 from .exceptions import PyPresenceException
 
 
-# Made by https://github.com/LewdNeko ;^)
 def remove_none(d: dict):
     for item in d.copy():
         if isinstance(d[item], dict):
@@ -30,7 +29,7 @@ def get_ipc_path(pipe=None):
 
     if sys.platform in ('linux', 'darwin'):
         tempdir = (os.environ.get('XDG_RUNTIME_DIR') or tempfile.gettempdir())
-        paths = ['.', 'snap.discord', 'app/com.discordapp.Discord']
+        paths = ['.', 'snap.discord', 'app/com.discordapp.Discord', 'app/com.discordapp.DiscordCanary']
     elif sys.platform == 'win32':
         tempdir = r'\\?\pipe'
         paths = ['.']
@@ -41,30 +40,17 @@ def get_ipc_path(pipe=None):
         full_path = os.path.abspath(os.path.join(tempdir, path))
         if sys.platform == 'win32' or os.path.isdir(full_path):
             for entry in os.scandir(full_path):
-                if entry.name.startswith(ipc):
+                if entry.name.startswith(ipc) and os.path.exists(entry):
                     return entry.path
 
 
 def get_event_loop(force_fresh=False):
-    if sys.platform in ('linux', 'darwin'):
-        if force_fresh:
-            return asyncio.new_event_loop()
-        loop = asyncio.get_event_loop()
-        if loop.is_closed():
-            return asyncio.new_event_loop()
-        return loop
-    elif sys.platform == 'win32':
-        if force_fresh:
-            return asyncio.ProactorEventLoop()
-        loop = asyncio.get_event_loop()
-        if isinstance(loop, asyncio.ProactorEventLoop) and not loop.is_closed():
-            return loop
-        return asyncio.ProactorEventLoop()
-
-
-# This code used to do something. I don't know what, though.
-try:  # Thanks, Rapptz :^)
-    create_task = asyncio.ensure_future
-except AttributeError:
-    create_task = getattr(asyncio, "async")
-    # No longer crashes Python 3.7
+    if force_fresh:
+        return asyncio.new_event_loop()
+    try:
+        running = asyncio.get_running_loop()
+    except RuntimeError:
+        return asyncio.new_event_loop()
+    if running.is_closed():
+        return asyncio.new_event_loop()
+    return running
