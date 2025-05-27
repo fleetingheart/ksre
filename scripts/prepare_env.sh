1b7bd58b4d7fe3687e8cf0dcc6079b6263baffe9
Vladimir Hodakov <vladimir@hodakov.me>
2025-05-27 05:05:07 +0400

    Okay, almost there

--- scripts/prepare_env.sh
+++ scripts/prepare_env.sh
@@ -3,7 +3,7 @@
 VERSION="v0.0.0-unknown"
 
 if [ -z "${CI_COMMIT_TAG}"]; then
-    VERSION="v0.0.0-${CI_COMMIT_SHA:0:10}-$(date +%s)"
+    VERSION="v0.0.0-$(date +%s)-${CI_COMMIT_SHA:0:10}"
 else
     VERSION=$CI_COMMIT_TAG
 fi
