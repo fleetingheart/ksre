7e58d4d9bf06fbc5ed2e812364a42ad9ff026269
Vladimir Hodakov <vladimir@hodakov.me>
2025-05-27 13:29:31 +0400

    Update env script

--- scripts/prepare_env.sh
+++ scripts/prepare_env.sh
@@ -4,9 +4,9 @@ VERSION="v0.0.0-unknown"
 
 if [ -z "${CI_COMMIT_TAG}"]; then
     PREVIOUS_TAG=`git -c 'versionsort.suffix=-' \
-        ls-remote --exit-code --refs --sort='version:refname' --tags \
-        https://codeberg.org/fhs/katawa-shoujo-re-engineered.git \
-        'v*.*.*' | tail --lines=1 | cut --delimiter='/' --fields=3`
+                    ls-remote --exit-code --refs --tags \
+                    'https://codeberg.org/fhs/katawa-shoujo-re-engineered.git' \
+                    'v*.*.*' | sort -k2 -V | tail -n 1 | cut -d '/' -f 3`
     VERSION="${PREVIOUS_TAG}-$(date +%s)-${CI_COMMIT_SHA:0:10}"
 else
     VERSION=$CI_COMMIT_TAG
