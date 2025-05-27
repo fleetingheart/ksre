68f9477ff643fbb9c89d72df98c16864ddc16974
Vladimir Hodakov <vladimir@hodakov.me>
2025-05-27 13:41:50 +0400

    Put version inside the game automatically

--- scripts/prepare_env.sh
+++ scripts/prepare_env.sh
@@ -12,10 +12,12 @@ if [ -z "${CI_COMMIT_TAG}"]; then
     MINOR=`echo $PREVIOUS_TAG | sed -e "s#$RE#\2#"`
     PATCH=`echo $PREVIOUS_TAG | sed -e "s#$RE#\3#"`
     let PATCH+=1
-    echo $PATCH
     VERSION="${MAJOR}.${MINOR}.${PATCH}-$(date +%s)-${CI_COMMIT_SHA:0:10}"
+    GAME_VERSION="${MAJOR}.${MINOR}.${PATCH}-${CI_COMMIT_SHA:0:10}"
 else
     VERSION=$CI_COMMIT_TAG
+    GAME_VERSION=$CI_COMMIT_TAG
 fi
 
 echo "CI_PRETTY_VERSION=${VERSION}" | tee .env
+echo "CI_KSRE_VERSION=${GAME_VERSION}" | tee .env
