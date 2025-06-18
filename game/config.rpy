05db9d2904f6dc04d84401e6d3d40eed371f2d84
Vladimir Hodakov <vladimir@hodakov.me>
2025-06-18 23:43:41 +0200

    Add Woodpecker CI to the repo (#177)
    This MR adds the ability to build KS:RE using Woodpecker CI, including all the platforms we are supporting at the moment (except iOS).
    
    Closes #57.
    
    Reviewed-on: https://codeberg.org/fhs/katawa-shoujo-re-engineered/pulls/177
    Co-authored-by: Vladimir Hodakov <vladimir@hodakov.me>
    Co-committed-by: Vladimir Hodakov <vladimir@hodakov.me>

--- game/config.rpy
+++ game/config.rpy
@@ -9,7 +9,7 @@ define config.developer = "auto"
 define config.window = "auto"
 
 define config.name = "Katawa Shoujo: Re-Engineered"
-define config.version = "2.0.2"
+define config.version = "v0.0.0-localbuild"
 
 define config.save_directory = "sh.fhs.ksre"
 
---
+++ scripts/prepare_env.sh
@@ -0,0 +1,23 @@
+#/bin/sh
+
+VERSION="v0.0.0-unknown"
+RE='[^0-9]*\([0-9]*\)[.]\([0-9]*\)[.]\([0-9]*\)\([0-9A-Za-z-]*\)'
+
+if [ -z "${CI_COMMIT_TAG}"]; then
+    PREVIOUS_TAG=`git -c 'versionsort.suffix=-' \
+                    ls-remote --exit-code --refs --tags \
+                    'https://codeberg.org/fhs/katawa-shoujo-re-engineered.git' \
+                    'v*.*.*' | sort -k2 -V | tail -n 1 | cut -d '/' -f 3`
+    MAJOR=`echo $PREVIOUS_TAG | sed -e "s#$RE#\1#"`
+    MINOR=`echo $PREVIOUS_TAG | sed -e "s#$RE#\2#"`
+    PATCH=`echo $PREVIOUS_TAG | sed -e "s#$RE#\3#"`
+    let PATCH+=1
+    VERSION="v${MAJOR}.${MINOR}.${PATCH}-${CI_PIPELINE_CREATED}-${CI_COMMIT_SHA:0:10}"
+    GAME_VERSION="${MAJOR}.${MINOR}.${PATCH}-${CI_COMMIT_SHA:0:4}"
+else
+    VERSION=$CI_COMMIT_TAG
+    GAME_VERSION=$CI_COMMIT_TAG
+fi
+
+echo "CI_PRETTY_VERSION=${VERSION}" | tee -a .env
+echo "CI_KSRE_VERSION=${GAME_VERSION}" | tee -a .env
