bbc56bed750753c59393fe6abfec87bc11ea0cc6
neparij <neparij@noreply.codeberg.org>
2025-07-02 23:01:56 +0200

    Update tl_videos_gen/font.sh

--- tl_videos_gen/font.sh
+++ tl_videos_gen/font.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Merge Playtime and custom font (jp,zh_hans)
-# Author Sehaxe
+# Author: Sehaxe
 fontforge -lang=ff -script merge.ff ../game/font/playtime.ttf ../game/font/VL-PGothic-Regular.ttf 1000 assets/jp.ttf
 fontforge -lang=ff -script merge.ff ../game/font/playtime.ttf ../game/font/XiaolaiSC-Regular.ttf 1000 assets/zh_hans.ttf
 rm -f temp-merge-font.ttf
